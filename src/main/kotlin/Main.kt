51d391e73a986154613da116cf146d1493371cb9
CristoferHernandez46 <crisitohernandez@gmail.com>
2022-08-24 08:38:38 -0600

    Suma Modo de Consola 24082022

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(){
+    println("Programa Cristofer Hernandez")
+    print("Ingrese un numero 1: ")
+    val n1 = readLine()!!.toInt()
+
+    print("Ingrese un numero 2: ")
+    val n2 = readLine()!!.toInt()
+
+    val resultado = n1 + n2
+    println("El resulado es: "+resultado)
+
+
+}
